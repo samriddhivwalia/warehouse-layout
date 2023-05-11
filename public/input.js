6509b78affee8007f7ff86122b565438207d5e79
Samriddhi V Walia <76689358+Sam-vw@users.noreply.github.com>
2023-05-11 19:13:33 +0530

    Minor fixes

--- public/input.js
+++ public/input.js
@@ -1,6 +1,6 @@
-export const WIDTH_OF_RACK = 10
-export const HEIGHT_OF_RACK = 10
-export const NUMBER_OF_RACK = 5
+export const WIDTH_OF_RACK = 5
+export const HEIGHT_OF_RACK = 5
+export const NUMBER_OF_RACK = 7 //number of lanes
 /*
 |    |
 |    |
@@ -8,7 +8,7 @@ export const NUMBER_OF_RACK = 5
 |____|
 */
 // the horizantal length in the above image
-export const WAREHOUSE_LENGTH = 100
+export const WAREHOUSE_LENGTH = 150
 
 // the vertical or depth in above image
 export const WAREHOUSE_DEPTH = 100
